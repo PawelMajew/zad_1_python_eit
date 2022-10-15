164c8b5ed34c3d13f036b7b02d9df5323d074ec1
pawel <pawelm2502@gmail.com>
2022-10-15 13:04:18 +0200

    zad_1 Hello World

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
+#Paweł Majewski EiT Programowanie w języku Python
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+#!/usr/bin/env python3
 
+def print_text(text):
+    print(f' {text}')
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_text('Hello World')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
