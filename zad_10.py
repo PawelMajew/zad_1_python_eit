8f869a1a297c93d9b8fbf74299e1a3dbb7a68c34
pawel <pawelm2502@gmail.com>
2022-10-16 11:05:05 +0200

    zad_9 zad_10 working task

--- zad_10.py
+++ zad_10.py
@@ -1,5 +1,32 @@
 #Paweł Majewski EiT Programowanie w języku Python
 #zad_10
+import random
+def bubbleSort(arr):
+    n = len(arr)
+    swapped = False
+    for i in range(n - 1):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                swapped = True
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
+        if not swapped:
+            return
+
+tab = []
 if __name__ == '__main__':
-    print("init")
+    for i in range(50):
+        tab.append(random.randint(1,200))
+
+    # print(tab)
+    temp_1 = tab
+    temp_2 = tab
+
+    bubbleSort(temp_1)
+    # print(temp_1)
+    temp_2.sort()
+    for i in range(50):
+        if(temp_1[i] != temp_2[i]):
+            print("not")
+    # print("\n")
+    #print(tab)
--- zad_9.py
+++ zad_9.py
@@ -1,5 +1,24 @@
 #Paweł Majewski EiT Programowanie w języku Python
 #zad_9
-
+#dodac możliwość wpisywania kilka razy i wykluczenie źle wpisanych liczb
+import math
 if __name__ == '__main__':
     print("init")
+    a, b, c = input("podaj wartosci rownania a, b, c\n").split()
+    print(a,b,c)
+    a=int(a)
+    b=int(b)
+    c=int(c)
+    if(a==0):
+        print("to nie jest rownanie kwadratowe\n")
+    if(a!=0):
+        delta = b*b-4*a*c
+        if(delta>0):
+            x1 =(-b-math.sqrt(delta))/(2*a)
+            x2 =(-b+math.sqrt(delta))/(2*a)
+            print(x1,x2)
+        if(delta<0):
+            print("brak rozwiazania\n")
+        if(delta==0):
+            x1= -b/(2*a)
+            print(x1)
