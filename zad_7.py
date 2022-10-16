c1e842878b47d8abd6bb96bd40bcb0cfd79d4b10
pawel <pawelm2502@gmail.com>
2022-10-16 10:03:11 +0200

    zad_7 working version

--- zad_7.py
+++ zad_7.py
@@ -2,4 +2,12 @@
 #zad_7
 
 if __name__ == '__main__':
-    print("init")
+    text_file = open("text_1.txt", "r+")
+
+    temp = text_file.read()
+    #trzeba dodać spacje na końcu słowa
+    vowel = ["i","jak","pojęcia"]
+    for i in vowel:
+        temp = temp.replace(i, "")
+
+    print(temp)
