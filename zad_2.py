2f184174d8f01c004dabeee7e2c7bad6016fa461
pawel <pawelm2502@gmail.com>
2022-10-16 13:26:43 +0200

    zad_2 ok

--- zad_2.py
+++ zad_2.py
@@ -1,5 +1,16 @@
 #Paweł Majewski EiT Programowanie w języku Python
 #zad_2
 
+def print_data(name, surname, date):
+    print("Your name= "+name+"\n")
+    print("Your surname= "+surname+"\n")
+    print("Your date= "+date+"\n")
+
 if __name__ == '__main__':
-    print("init")
+    while 1:
+        try:
+            name, surname, date = input("podaj imie, nazwisko, rok urodzenia\n").split()
+            break;
+        except ValueError:
+            print("zle dane, jeszcze raz\n")
+    print_data(name,surname,date)
