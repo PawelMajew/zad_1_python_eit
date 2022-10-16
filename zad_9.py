5208dfa46d25e8fdae48e2b46bd72409eda0aeec
pawel <pawelm2502@gmail.com>
2022-10-16 18:27:33 +0200

    zad_9 ok

--- zad_9.py
+++ zad_9.py
@@ -1,24 +1,37 @@
 #Paweł Majewski EiT Programowanie w języku Python
 #zad_9
-#dodac możliwość wpisywania kilka razy i wykluczenie źle wpisanych liczb
+
 import math
-if __name__ == '__main__':
-    print("init")
-    a, b, c = input("podaj wartosci rownania a, b, c\n").split()
-    print(a,b,c)
-    a=int(a)
-    b=int(b)
-    c=int(c)
-    if(a==0):
+def get_value():
+    return  input("podaj wartosci rownania a, b, c\n")
+def quadratic_equation(abc):
+    a = int(abc[0])
+    b = int(abc[1])
+    c = int(abc[2])
+    print(abc)
+    if(a == 0):
         print("to nie jest rownanie kwadratowe\n")
-    if(a!=0):
-        delta = b*b-4*a*c
-        if(delta>0):
-            x1 =(-b-math.sqrt(delta))/(2*a)
-            x2 =(-b+math.sqrt(delta))/(2*a)
+    if(a != 0):
+        delta = b * b - 4 * a * c
+
+        if(delta > 0):
+            x1 = (-b - math.sqrt(delta)) / (2 * a)
+            x2 = (-b + math.sqrt(delta)) / (2 * a)
             print(x1,x2)
-        if(delta<0):
+
+        if(delta < 0):
             print("brak rozwiazania\n")
-        if(delta==0):
-            x1= -b/(2*a)
+
+        if(delta == 0):
+            x1 = -b / (2 * a)
             print(x1)
+
+if __name__ == '__main__':
+
+    while 1:
+        try:
+            temp = get_value()
+            quadratic_equation(temp.split())
+            break;
+        except ValueError:
+            print("zle wartosci jeszcze raz\n")
