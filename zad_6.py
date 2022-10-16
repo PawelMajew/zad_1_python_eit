06c14749d3705349056a2e4633e716344cc0dea2
pawel <pawelm2502@gmail.com>
2022-10-16 13:45:43 +0200

    zad_6 ok

--- zad_6.py
+++ zad_6.py
@@ -1,5 +1,8 @@
 #Paweł Majewski EiT Programowanie w języku Python
 #zad_6
+from PIL import Image
 
 if __name__ == '__main__':
-    print("init")
+
+    im1 = Image.open(r"E:\studia\semestr_VII\python\cw_labo\zad\lion.jpg")
+    im1.save(r"E:\studia\semestr_VII\python\cw_labo\zad\lion.png")
