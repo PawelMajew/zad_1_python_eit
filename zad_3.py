0894787c9892bf0656eb223dfb4f8a90bbd0cd6f
pawel <pawelm2502@gmail.com>
2022-10-16 13:37:36 +0200

    zad_3 ok

--- zad_3.py
+++ zad_3.py
@@ -1,5 +1,17 @@
 #Paweł Majewski EiT Programowanie w języku Python
 #zad_3
 
+def get_code():
+    return input("ustaw kod\n")
+def check_code(code_true):
+    while 1:
+        input_code = input("podaj kod\n")
+        if(input_code == code_true):
+            print("wlasciwy kod\n")
+            break;
+        else:
+            print("zly kod!!\n")
+
 if __name__ == '__main__':
-    print("init")
+    code_true = get_code()
+    check_code(code_true)
