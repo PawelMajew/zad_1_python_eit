afe63452af067061e1f76ae80d60f4ac8815fc50
pawel <pawelm2502@gmail.com>
2022-10-16 18:43:21 +0200

    zad 11 ok

--- zad_11.py
+++ zad_11.py
@@ -1,11 +1,18 @@
 #Paweł Majewski EiT Programowanie w języku Python
 #zad_11
+def matrix_multiplication(a, b):
+    c = 0
+    if len(a) == len(b):
+        for i in range(0, len(a)):
+            c += a[i] * b[i]
+        print(c)
+    else:
+        print("zly rozmiar")
+
 
 if __name__ == '__main__':
+
     a = [1, 2, 12, 4]
     b = [2, 4, 2, 8]
-    c = 0
-    for i in range(0,len(a)):
-        c += a[i]*b[i]
 
-    print(c)
+    matrix_multiplication(a,b)
