658db91fd1d115207dc8f5e32069293656e2203b
pawel <pawelm2502@gmail.com>
2022-10-17 17:28:19 +0200

    zad_4 ok

--- zad_4.py
+++ zad_4.py
@@ -1,6 +1,29 @@
 #Paweł Majewski EiT Programowanie w języku Python
 #zad_4
+# Napisz skrypt zliczający ilość plików w katalogu /dev, skorzystaj
+# ze standardowej biblioteki - os
+
 import os
+
+
+def show_count(path):
+    with open('FileData_count.txt', 'w') as  file_output:
+        # with path.open('r') as f
+
+        for path, subfiles, files in os.walk(path):
+            file_output.write('=' * 70 + '\n')
+            file_output.write(path + '\n')
+            if len(files):
+                count = 0
+                for i in files:
+                    count += 1
+                file_output.write("ilosc plikow: " + str(count) + '\n')
+            else:
+                file_output.write("ilosc plikow: 0\n")
+    # file_output.close()
+
+
 if __name__ == '__main__':
-    path = "cos"
-    temp_dir = os.listdir(path)
+
+    path = "E:\studia\semestr_VII\python\cw_labo\zad"
+    show_count(path)
