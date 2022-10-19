6eaa835712845c7478d2ce960b85a0264b4226a6
pawel <pawelm2502@gmail.com>
2022-10-19 19:29:30 +0200

    zad 7 done

--- zad_7.py
+++ zad_7.py
@@ -1,13 +1,22 @@
 #Paweł Majewski EiT Programowanie w języku Python
 #zad_7
+# Napisz skrypt usuwający z wejściowego ciągu tekstowego
+# (wybierz kilka plików z repozytorium: Tekstowego) następujące
+# słowa : się, i, oraz, nigdy, dlaczego
 
-if __name__ == '__main__':
-    text_file = open("text_1.txt", "r+")
 
-    temp_text = text_file.read()
+def delete_word(file):
+    temp_text = file.read()
+    print(temp_text + '\n')
 
-    list = ["a ","ku ","widzi "]
+    list = ["a ", "ku ", "widzi "]
     for next_element in list:
         temp_text = temp_text.replace(next_element, "")
 
-    print(temp_text)
+    print(temp_text + '\n')
+
+
+if __name__ == '__main__':
+
+    text_file = open("text_1.txt", "r")
+    delete_word(text_file)
