a31884ea3eee61dbe1a9d2d7b48f58d5d3a6bb1d
pawel <pawelm2502@gmail.com>
2022-10-19 18:23:37 +0200

    zad 6 done

--- zad_6.py
+++ zad_6.py
@@ -1,8 +1,10 @@
 #Paweł Majewski EiT Programowanie w języku Python
 #zad_6
+# Napisz skrypt konwersji rozszerzeń plików *.jpg na *.png (uprzednio stwórz zestaw 4 plików z rozszerzeniem *.jpg)
 from PIL import Image
+
 
 if __name__ == '__main__':
 
-    im1 = Image.open(r"E:\studia\semestr_VII\python\cw_labo\zad\lion.jpg")
-    im1.save(r"E:\studia\semestr_VII\python\cw_labo\zad\lion.png")
+    image = Image.open(r"E:\studia\semestr_VII\python\cw_labo\zad\lion.jpg")
+    image.save(r"E:\studia\semestr_VII\python\cw_labo\zad\lion.png")
