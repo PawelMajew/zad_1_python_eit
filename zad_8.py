5e3c90471e24be89106e89c08cdb3bd20a7b20e7
pawel <pawelm2502@gmail.com>
2022-10-19 20:13:24 +0200

    zad 8 done

--- zad_8.py
+++ zad_8.py
@@ -1,18 +1,21 @@
 #Paweł Majewski EiT Programowanie w języku Python
 #zad_8
+# Napisz skrypt zmieniający w podanym ciągu wejściowym
+# (wybierz kilka plików z repozytorium: Tekstowego) następujące
+# słowa : i, oraz, nigdy, dlaczego na następujący zestaw słów : oraz,
+# i, prawie nigdy, czemu. Zalecaną strukturą jest słownik.
+
+def replace_word(file):
+    temp_text = file.read()
+    slownik = {' a ': 'tak ', ' ku ': 'ale ', ' widzi ': 'patrzy '}
+    print(temp_text+'\n')
+
+    for next_element in slownik:
+        temp_text = temp_text.replace(next_element, slownik[next_element])
+
+    print(temp_text)
+
 
 if __name__ == '__main__':
     text_file = open("text_1.txt", "r+")
-
-    temp_text = text_file.read()
-
-    list = ["a ","ku ","widzi "]
-    for next_element in list:
-        if next_element == list[0]:
-            temp_text = temp_text.replace(next_element, "ale ")
-        elif next_element == list[1]:
-            temp_text = temp_text.replace(next_element, "tak ")
-        elif next_element == list[2]:
-            temp_text = temp_text.replace(next_element, "patrzy ")
-
-    print(temp_text)
+    replace_word(text_file)
