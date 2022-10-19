996e3a6c1c74ca361702b3182952e89fae5ccae5
pawel <pawelm2502@gmail.com>
2022-10-19 18:07:31 +0200

    zad_2 done

--- zad_2.py
+++ zad_2.py
@@ -1,16 +1,23 @@
 #Paweł Majewski EiT Programowanie w języku Python
 #zad_2
+# Napisz skrypt, który pyta o imię, nazwisko i rok urodzenia
+# (powinny być podane w jednej linii)
 
 def print_data(name, surname, date):
     print("Your name= "+name+"\n")
     print("Your surname= "+surname+"\n")
     print("Your date= "+date+"\n")
 
-if __name__ == '__main__':
+
+def get_and_print():
     while 1:
         try:
             name, surname, date = input("podaj imie, nazwisko, rok urodzenia\n").split()
-            break;
+            break
         except ValueError:
             print("zle dane, jeszcze raz\n")
     print_data(name,surname,date)
+
+
+if __name__ == '__main__':
+    get_and_print()
