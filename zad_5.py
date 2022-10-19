2c3bdb73350b68f2354b4a9d6a167882f032f7dc
pawel <pawelm2502@gmail.com>
2022-10-19 18:21:19 +0200

    zad_5 done

--- zad_5.py
+++ zad_5.py
@@ -1,5 +1,24 @@
 #Paweł Majewski EiT Programowanie w języku Python
 #zad_5
+# Napisz rekurencyjne przejście drzewa katalogów i wypisanie
+# plików, które znajdują się w eksplorowanej strukturze
+import os
+
+
+def show(path):
+
+    for item in os.listdir(path):
+
+        new_path = os.path.join(path, item);
+        print("\n")
+        print(new_path)
+        print(("*")*70+"\n")
+        print(item)
+        if os.path.isdir(new_path):
+            show(new_path)
+
 
 if __name__ == '__main__':
-    print("init")
+
+    path = "E:\studia\semestr_VII\python\cw_labo\zad"
+    show(path)
