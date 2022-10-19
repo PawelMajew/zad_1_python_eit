02ee7430aa9be2e527c25e55a2201b70bd6828b9
pawel <pawelm2502@gmail.com>
2022-10-19 18:09:42 +0200

    zad_3 done

--- zad_3.py
+++ zad_3.py
@@ -1,17 +1,23 @@
 #Paweł Majewski EiT Programowanie w języku Python
 #zad_3
-
+# Napisz skrypt realizujący funkcję zamka szyfrowego. Prosi
+# o podanie kodu i następnie sprawdza czy jest on zgodny z
+# wcześniej wprowadzonym kodem
 def get_code():
     return input("ustaw kod\n")
+
+
 def check_code(code_true):
     while 1:
         input_code = input("podaj kod\n")
-        if(input_code == code_true):
+        if input_code == code_true:
             print("wlasciwy kod\n")
-            break;
+            break
         else:
             print("zly kod!!\n")
 
+
 if __name__ == '__main__':
+
     code_true = get_code()
     check_code(code_true)
