895ec836d0d223f4f20b82c4481d3c6283c3564e
pawel <pawelm2502@gmail.com>
2022-10-19 18:07:04 +0200

    zad_20 todo

--- zad_20.py
+++ zad_20.py
@@ -1,5 +1,6 @@
 #Paweł Majewski EiT Programowanie w języku Python
 #zad_20
+# Zaimplementuj prosty problem pięciu filozofów (z deadlockiem), następnie usuń deadlock
 
 if __name__ == '__main__':
     print("init")
