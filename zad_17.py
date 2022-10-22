5669c08853b2ed16b026927639a12a8c9c864109
pawel <pawelm2502@gmail.com>
2022-10-22 20:17:45 +0200

    zad 17 done

--- zad_17.py
+++ zad_17.py
@@ -1,5 +1,24 @@
 #Paweł Majewski EiT Programowanie w języku Python
 #zad_17
+# Sparsuj przygotowanego XML (parserem SAX i DOM) i go
+# zmodyfikuj np. zmień wartość któregoś tag’a i zapisz do nowego
+# pliku
+from xml.dom.minidom import parse
+import xml.dom.minidom
+
+
+def parse_and_change_tag(file,tag,new_tag):
+    DOMTree = xml.dom.minidom.parse(file)
+    collection = DOMTree.documentElement
+    movies = collection.getElementsByTagName(tag)
+
+    for i in movies:
+        movies = collection.getElementsByTagName(tag)[0]
+        movies.tagName = new_tag
+
+    with open("new_file.xml", "wb") as f:
+        f.write(collection.toxml("utf-8"))
+
 
 if __name__ == '__main__':
-    print("init")
+    parse_and_change_tag("new.xml", "movie", "newTag")
